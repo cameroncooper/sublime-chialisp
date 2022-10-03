e205f6fa4da66b74774292aac8dce5b886396d8c
Cameron Cooper <cameron@chia.net>
2022-10-03 12:47:09 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Chialisp
+
+Sublime Text plugin for [Chialisp](https://github.com/typescript-language-server/typescript-language-server) using the [Chialisp Language Server](https://github.com/Chia-Network/vscode-chialisp-lsp).
+
+## Installation
+
+ * Install [`LSP`](https://packagecontrol.io/packages/LSP) and `Chialisp` from Package Control.
+ * Restart Sublime.
+
+## Configuration
+
+Open the configuration file using the Command Palette `Preferences: Chialisp Settings` command or open it from the Sublime menu.
---
+++ plugin.py
@@ -0,0 +1,25 @@
+import os
+import sublime
+import sublime_plugin
+from LSP.plugin.core.typing import Tuple
+from lsp_utils import NpmClientHandler
+from lsp_utils import request_handler
+
+
+def plugin_loaded() -> None:
+    LspChialispPlugin.setup()
+
+
+def plugin_unloaded() -> None:
+    LspChialispPlugin.cleanup()
+
+
+class LspChialispPlugin(NpmClientHandler):
+    package_name = __package__
+    server_directory = 'chialisp-language-server'
+    server_binary_path = os.path.join(server_directory, 'runner', 'runner.js')
+    skip_npm_install = True
+
+    @classmethod
+    def minimum_node_version(cls) -> Tuple[int, int, int]:
+        return (14, 16, 0)
